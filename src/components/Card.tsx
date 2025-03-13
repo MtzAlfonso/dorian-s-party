e2b68d1fd6ecbfdee4492b79187c94965a87732a
J Alfonso Martinez <mtz.alfonso96@gmail.com>
2025-03-13 15:30:59 -0600

    fix: :bug: update button hover text color in Card component for confirmation states

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -14,7 +14,6 @@ export const Card: FC<ICardProps> = ({ member, onConfirm, loading }) => {
   const [stateMember, setStateMember] = useState<IMember>(member);
 
   const handleConfirmation = (confirmation: boolean) => {
-    if (stateMember.confirmation === confirmation) return;
     onConfirm(stateMember.name, confirmation);
     setStateMember({ ...stateMember, confirmation });
   };
@@ -37,7 +36,7 @@ export const Card: FC<ICardProps> = ({ member, onConfirm, loading }) => {
             'p-2 h-10 w-10 rounded-md cursor-pointer',
             stateMember.confirmation
               ? 'bg-teal-600 text-white hover:bg-teal-700'
-              : 'border border-teal-600 text-teal-600 hover:bg-teal-600 hover:text',
+              : 'border border-teal-600 text-teal-600 hover:bg-teal-600 hover:text-white',
           ])}
         >
           <MdCheck className="text-xl" />
@@ -47,7 +46,7 @@ export const Card: FC<ICardProps> = ({ member, onConfirm, loading }) => {
           className={twMerge([
             'p-2 h-10 w-10 rounded-md cursor-pointer',
             stateMember.confirmation
-              ? 'border border-red-600 text-red-600 hover:bg-red-600 hover:text-red-600'
+              ? 'border border-red-600 text-red-600 hover:bg-red-600 hover:text-white'
               : 'bg-red-600 text-white hover:bg-red-700',
           ])}
         >
